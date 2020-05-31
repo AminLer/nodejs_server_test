77e7e475f96c30defadb1bd4b4aed47400b405fb
AminLer <aminoumhamdi@gmail.com>
2020-05-31 03:40:36 +0200

    stuff

--- webserver.js
+++ webserver.js
@@ -6,7 +6,7 @@ var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
 http.listen(8080); //listen to port 8080
-console.log("Server running...")
+console.log("Server running...");
 function handler (req, res) { //create server
   fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
     if (err) {
